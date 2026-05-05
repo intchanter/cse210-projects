8cf9285a132a112e877a3e2b24ef283223b08207
Daniel Fackrell <unlearned@gmail.com>
2026-05-04 22:41:15 -0600

    Finished week01, prep01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        string first = Input("What is your first name? ");
+        string last = Input("What is your last name? ");
+        Console.WriteLine($"\nYour name is {last}, {first} {last}.");
+    }
+
+    static string Input(string prompt = "")
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
     }
 }
