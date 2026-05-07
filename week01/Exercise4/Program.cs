e31d6280734d1da36c04b87629b7a57a1f252a58
Daniel Fackrell <unlearned@gmail.com>
2026-05-06 21:44:58 -0600

    Finished week01, exercise 4, including stretch challenges

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of integers, type 0 when finished.");
+        while (true)
+        {
+            int number = Input.Integer("Enter integer: ");
+            if (number == 0)
+            {
+                break;
+            }
+            numbers.Add(number);
+        }
+
+        int sum = 0;
+        int largest = int.MinValue;
+        int smallest = int.MaxValue;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number < smallest && number > 0)
+            {
+                smallest = number;
+            }
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        float mean = sum / (float) numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The mean is: {mean}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+        Console.WriteLine($"The largest number is: {largest}");
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
+
+class Input
+{
+    public static int Integer(string prompt)
+    {
+        Console.Write(prompt);
+        return int.Parse(Console.ReadLine());
+    }
+    public static string String(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
     }
 }
