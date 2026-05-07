9d3acf13112bff606629b5ae66081ab24e6a0371
Daniel Fackrell <unlearned@gmail.com>
2026-05-06 20:56:01 -0600

    Solved week 1 exercise 3, including stretch challenges

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        do
+        {
+            int magicNumber = randomGenerator.Next(1, 11);
+            int guesses = 0;
+            
+            while (true)
+            {
+                int guess = Input.Integer("What is your guess? ");
+                guesses += 1;
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.WriteLine($"You used {guesses} guesses");
+        } while (Input.String("Would you like to play again? (y/n) ").ToLower() == "y");
+    }
+}
+
+class Input
+{
+    static public int Integer(string prompt)
+    {
+        Console.Write(prompt);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static public string String(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
     }
 }
