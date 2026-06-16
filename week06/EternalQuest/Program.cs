d953efae45197d40e1c0cd804e0400894a0cb94c
Daniel Fackrell <unlearned@gmail.com>
2026-06-15 22:52:53 -0600

    Corrected minor typo in enhancements

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -2,7 +2,7 @@
 // * Used Json Lines format for saving the current score and each goal.
 // * Implemented polymorphic serialization and deserialization using
 //   JsonSerializer.
-// * Added levels based on 100 * triangle numbers
+// * Added levels based on 100 * triangle numbers.
 
 class Program
 {
